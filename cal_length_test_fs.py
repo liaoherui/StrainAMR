1cb5850d503f7a2969c603e78b891647686f1954
Ray <851836818@qq.com>
2025-09-22 13:23:36 -0400

    Update cal_length_test_fs.py

--- cal_length_test_fs.py
+++ cal_length_test_fs.py
@@ -93,7 +93,8 @@ def run_cdhit(ptrain,pval,work_dir):
 def output_pc_token_file(d,pdir,label,ofile,idx):
     dr={} # Strain prefix -> Tokens string
     for filename in os.listdir(pdir):
-        pre=re.split('\.',filename)[0]
+        #pre=re.split('\.',filename)[0]
+        pre=os.path.splitext(filename)[0]
         if pre not in dr:
             dr[pre]=[]
         f=open(pdir+'/'+filename,'r')
@@ -197,7 +198,8 @@ def run_ps(train,val,ingenome,label,drug,work_dir):
     dtrain={} # Pre -> Genome dir
     dval={}
     for filename in os.listdir(ingenome):
-        pre=re.split('\.',filename)[0]
+        #pre=re.split('\.',filename)[0]
+        pre=os.path.splitext(filename)[0]
         if pre in train:
             dtrain[pre]=ingenome+'/'+filename
         elif pre in val:
@@ -290,7 +292,8 @@ def scan_length_fs_shap_topx(odir):
 def run(ingenome,label,odir,drug,mfile):
     dr={}
     for filename in os.listdir(ingenome):
-        pre=re.split('\.',filename)[0]
+        #pre=re.split('\.',filename)[0]
+        pre=os.path.splitext(filename)[0]
         #print(filename)
         #print(pre)
         #exit()
